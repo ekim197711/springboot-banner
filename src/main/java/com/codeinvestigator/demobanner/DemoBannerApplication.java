e6fbeaf5105bd4919cbd5d1be83e9311f320cd66
Mike Møller Nielsen <mmni@protonmail.com>
2019-08-11 23:19:57 +0200

    first commit

---
+++ src/main/java/com/codeinvestigator/demobanner/DemoBannerApplication.java
@@ -0,0 +1,13 @@
+package com.codeinvestigator.demobanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBannerApplication.class, args);
+    }
+
+}
